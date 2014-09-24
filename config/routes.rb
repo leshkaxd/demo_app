5f5f5683375fae745e7c43d5c17d88475ba3e80d
Alexey Leontyev <leshka.xd@gmail.com>
2014-09-24 12:31:34 +0400

    finish layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  match '/about',   to: 'pages#about',   via: 'get'
-  match '/help',    to: 'pages#help',    via: 'get'
-  match '/contact', to: 'pages#contact', via: 'get'
+  get "users/new"
+  get '/signup',  to: 'users#new'
 
-  resources :microposts
-
-  resources :users
+  get '/about',   to: 'pages#about'
+  get '/help',    to: 'pages#help'
+  get '/contact', to: 'pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
