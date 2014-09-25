8bd5788db358f234a2ec81d7a2fb0f897decf70a
Alexey Leontyev <leshka.xd@gmail.com>
2014-09-25 18:24:48 +0400

    finish modeling User (with sequre password)

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ group :test do
   gem 'capybara', '2.1.0'
 end
 
+gem 'bcrypt-ruby', '3.1.2'
 gem 'sass-rails', '~> 4.0.3'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'uglifier', '>= 1.3.0'
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w\+\-\.]+@[\w\-]+(\.\w+)*\.\w+\z/i
+
+  has_secure_password
+
+  before_save { self.email.downcase! }
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+end
